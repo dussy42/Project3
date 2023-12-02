e6ea0b2451327aba28e176f4ebd73b12480101d6
dussy42 <98059288+dussy42@users.noreply.github.com>
2023-12-02 04:00:19 +0100

    Update run.py
    Adding function that gets the ship's location

--- run.py
+++ run.py
@@ -12,3 +12,16 @@ def print_board(board):
     for row in board:
         print("%d|%s|" % (row_num, "|".join(row)))
         row_num +=1
+
+def get_ship_location():
+    
+    row=input('Please enter a ship row 1-8 ').upper()
+    while row not in '12345678':
+        print("Please enter a valid row ")
+        row=input('Please enter a ship row 1-8 ')
+    
+    column=input('Please enter a ship column A-H ').upper()
+    while column not in 'ABCDEFGH':
+        print("Please enter a valid column ")
+        column=input('Please enter a ship column A-H ')
+    return int(row)-1,let_to_num[column]
